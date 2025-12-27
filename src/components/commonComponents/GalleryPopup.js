54511cdd404edb583f51b5094f181552865f7fc4
Deepak4511 <deepaksingh451181@gmail.com>
2025-12-27 22:37:55 +0530

    updated code

--- src/components/commonComponents/GalleryPopup.js
+++ src/components/commonComponents/GalleryPopup.js
@@ -1,15 +1,14 @@
-
 import React, { useState } from "react";
 import Image from "next/image";
 import { IoMdClose } from "react-icons/io";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const GalleryPopup = ({ images, selectedIndex, onClose }) => {
-      const [currentIndex, setCurrentIndex] = useState(selectedIndex);
+  const [currentIndex, setCurrentIndex] = useState(selectedIndex);
 
   const goNext = () => setCurrentIndex((prev) => (prev + 1) % images.length);
-  const goPrev = () => setCurrentIndex((prev) => (prev - 1 + images.length) % images.length);
-
+  const goPrev = () =>
+    setCurrentIndex((prev) => (prev - 1 + images.length) % images.length);
 
   return (
     <div
@@ -22,77 +21,64 @@ const GalleryPopup = ({ images, selectedIndex, onClose }) => {
         <div className="modal-content  position-relative popup-box galleryPopup_box overflow-hidden">
           <button className="close-btn z-1 bg-white" onClick={onClose}>
             <IoMdClose size={22} />
-            </button>
-
-
-
-            {/* image */}
-
-      <div className="position-relative bg-white rounded-4 shadow-lg p-3">
-
-
-        {/* Image Navigation */}
-        <div className="">
-          <button
-            className="btn btn-light gallery_left_btn position-absolute translate-middle-y"
-            style={{ top: "50%" }}
-            onClick={goPrev}
-          >
-            <ChevronLeft size={20} />
           </button>
 
-          <Image
-            src={images[currentIndex]}
-            alt={`Gallery ${currentIndex + 1}`}
-            width={766}
-            height={532}
-            className="rounded-4 mx-auto w-100 popup-image"
-          />
+          {/* image */}
 
-          <button
-            className="btn btn-light gallery_right_btn position-absolute  translate-middle-y"
-            style={{ top: "50%" }}
-            onClick={goNext}
-          >
-            <ChevronRight size={20} />
-          </button>
-        </div>
+          <div className="position-relative bg-white rounded-4 shadow-lg p-3">
+            {/* Image Navigation */}
+            <div className="">
+              <button
+                className="btn btn-light gallery_left_btn position-absolute translate-middle-y"
+                style={{ top: "50%" }}
+                onClick={goPrev}
+              >
+                <ChevronLeft size={20} />
+              </button>
 
-        {/* Thumbnail Scroll */}
-        <div
-          className="d-flex justify-content-center gap-2 mt-3 overflow-auto"
-        >
-          {images.map((thumb, i) => (
-            <Image
-              key={i}
-              src={thumb}
-              alt={`thumb-${i}`}
-              width={50}
-              height={50}
-              className={` border  ${
-                currentIndex === i ? "border-primary" : "border-transparent"
-              }`}
-              onClick={() => setCurrentIndex(i)}
-            />
-          ))}
+              <Image
+                // src={images[currentIndex]}
+                // alt={`Gallery ${currentIndex + 1}`}
+                // width={766}
+                // height={532}
+                // className="rounded-4 mx-auto w-100 popup-image"
+                src={images[currentIndex].image}
+                alt={images[currentIndex].alt || "Gallery image"}
+                width={1000}
+                height={1000}
+                className="rounded-4 mx-auto w-100 h-auto popup-image"
+              />
+
+              <button
+                className="btn btn-light gallery_right_btn position-absolute  translate-middle-y"
+                style={{ top: "50%" }}
+                onClick={goNext}
+              >
+                <ChevronRight size={20} />
+              </button>
+            </div>
+
+            {/* Thumbnail Scroll */}
+            <div className="d-flex justify-content-center gap-2 mt-3 overflow-auto">
+              {images.map((item, i) => (
+                <Image
+                  key={i}
+                  src={item.image}
+                  alt={item.alt || `thumb-${i}`}
+                  width={100}
+                  height={100}
+                  className={` border w-auto h-auto  ${
+                    currentIndex === i ? "border-primary" : "border-transparent"
+                  }`}
+                  onClick={() => setCurrentIndex(i)}
+                />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-
-
-
-
-
-
-
-
-          
-        </div>
-      </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default GalleryPopup
-
+export default GalleryPopup;
